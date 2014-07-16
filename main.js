748532e04c759f3e935f21f1020377f444466f97
brandoshmando <brancraft@gmail.com>
2014-07-15 22:32:46 -0400

    Finished laser object, refactored some css within the objects

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 //Instantiates new ship object on page ready
 $(document).ready(function(){
   var ship = new Ship().render();
-  new Laser().render();
-  var laserCount = 0
+
 //Ship Object
   function Ship(){
     this.x = 300
     this.y = -50
     this.render = function(){
       this.$me =$('<div id="ship"><img src="galaga.png" alt="http://www.quickmeme.com/meme/3ugwv5"></div>')
-      .css('left', this.x)
-      .css('top', this.y)
-      .css('position', 'relative');
-      console.log(this.$me)
+      .css({
+        left: this.x,
+        top: this.y,
+        position: 'relative',
+      });
       $('#game-wrap').append(this.$me)
     };
   };
@@ -20,21 +20,22 @@ $(document).ready(function(){
   function Laser(){
     this.x = $('#ship').position().left + 14
     this.y = 450 //$('#ship').position().top
-    this.render = function(){
+    this.fire = function(){
       this.$me = $('<div class="laser"></div>')
-      .css('height', '5px')
-      .css('width', '0px')
-      .css('left', this.x)
-      .css('top', this.y)
-      .css('border', '1px solid red')
-      .css('postion', 'absolute')
+      .css({
+        height: '5px',
+        width: '0px',
+        left: this.x,
+        top: this.y,
+        border: '1px solid red',
+        position: 'absolute'
+      });
       $('#game-wrap').append(this.$me)
+      this.$me.animate({top: 0}, 200).remove
     };
   };
 //Listens for keydown event and executes code to move ship left and right
   $(document).keydown(function(ev){
-    // console.log("triggered");
-    // console.log(ev.which)
     switch(ev.which){
       case 65:
         $('#ship').animate({left: 0}, calculateSpeed('L'), 'linear');
@@ -44,19 +45,7 @@ $(document).ready(function(){
         $('#ship').animate({left: "670"}, calculateSpeed('R'),  'linear');
         break;
       case 87:
-        // new Laser().fire();
-        laser = $('<div class="laser"></div>')
-        .css("height", "5px")
-        .css("width", "0px")
-        .css("border", "1px solid red")
-        .css("left", $('#ship').position().left + 14)
-        .css("top", "500px")
-        .css("position", "absolute")
-        $('#game-wrap').append(laser)
-        // console.log(laser)
-        laser.animate({top: 50}, 200, function(){
-          $(this).remove;
-          });
+        new Laser().fire();
         break;
     };
   });
@@ -85,111 +74,3 @@ $(document).ready(function(){
     return animDur;
   };
 })
-
-
-
-
-//The landfill of misguided code...
-
-
-// function Laser(){
- //    this.x = $('#ship').position().left
- //    this.y = 490
- //    this.height = 10
-
- //    this.fire = function(){
- //      var laser = $('<div id="laser></div>')
- //      .css('left', this.x)
- //      .css('top', this.y)
- //      .css('height', this.height)
- //      .css('border', '1px solid red')
- //      .css('position', 'absolute')
- //      $('game-wrap').append(laser)
- //      console.log(laser)
- //      laser.animate({top: "+=1000"})
- //    };
- //  };
-
-//   setInterval(moveShip, 1000); //Checks keys every 2 miliseconds to replicate held down key
-//   var keys = {};
-// //Pushes depressed key into keys and sets to true
-//   $( document ).keydown(function(ev){
-//   keys[ev.keyCode] = true;
-//   console.log('pressed!')
-//   });
-// //Removes same key when key is released
-//   $( document ).keyup(function(ev){
-//   delete keys[ev.keyCode];
-//   });
-
-// //Moves ship using keys object
-//   function moveShip(){
-//     console.log('movin...')
-//     for (var direction in keys){
-//       // if (!keys.hasOwnProperty(direction)){ //Stops from comparing to empty object.
-//         console.log('made it')
-//         if (direction  == 65) {
-//           $('#ship').animate({left: "-=5"}, 0);
-//         }if (direction  == 68) {
-//           $('#ship').animate({left: "-=5"}, 0);
-//         };
-//       };
-//     // };
-//   };
-// })
-
-//   $('window').on('keydown', function(ev){
-//     if (ev.keyCode === 65){
-//       ship.animate({
-//         left: "-= 5"
-//       })
-//     }else if(ev.keyCode === 68){
-//       left: "+= 5"
-//     };
-//   });
-// })
-// Cirlcle 1
-
-// $('document').ready(function(){
-//   var someCircles = [];
-//   for (var i=0; i<10; i++){
-//     someCircles.push(new Circle());
-//     someCircles[i].render();
-//   };
-// })
-// function Circle(){
-//   this.x = Math.random()*450;
-//   this.y = Math.random()*450;
-//   this.diameter = 30 + Math.random() * 50;
-//   this.speed = 500 + Math.random() * 1500;
-//   this.render = function(){
-//     this.$me = $('<div class="circles"></div>')
-//     .css('left', this.x)
-//     .css('top', this.y)
-//     .css('height', this.diameter)
-//     .css('width', this.diameter);
-//     $('#wrapper').append(this.$me)
-//   };
-// };
-
-// $('.circle').animate({
-//   top: "+=300px",
-//   left: "+=300px"
-// }, {
-//   complete: function() {
-//     alert("I moved");
-//   }
-// });
-
-
-// this.kill = function(){
-//   this.$me.css('backgroun-color', 'red').effect({
-//     effect: 'explode',
-//     duration: 100,
-//     complete: function(){
-//       $(this).remove();
-//     }, {
-//       queue: false;
-//     }
-//   });
-// }
