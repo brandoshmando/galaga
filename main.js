07776e4e5d5a9db341155abdb2e7bc6e4f593fa6
brandoshmando <brancraft@gmail.com>
2014-07-15 23:26:03 -0400

    Added an alien ship object and generated 30 of them in random places on page load

--- main.js
+++ main.js
@@ -1,25 +1,54 @@
 //Instantiates new ship object on page ready
 $(document).ready(function(){
-  var ship = new Ship().render();
-
+  new Ship().render();
+  aliensAttack();
 //Ship Object
   function Ship(){
     this.x = 300
-    this.y = -50
+    this.y = 465
     this.render = function(){
       this.$me =$('<div id="ship"><img src="galaga.png" alt="http://www.quickmeme.com/meme/3ugwv5"></div>')
       .css({
         left: this.x,
         top: this.y,
-        position: 'relative',
+        position: 'absolute',
       });
       $('#game-wrap').append(this.$me)
+    };
+  };
+
+//Alien Ship object
+  function Alien(x,y){
+    this.x = x
+    this.y = y
+    this.render = function(){
+      this.$me = $('<div class="alien-ship"><img src="galaga_alien.png"</div>')
+      .css({
+        top: this.x,
+        left: this.y,
+        width: '30px',
+        height: '30px',
+        position: 'absolute'
+      });
+      $('#game-wrap').append(this.$me);
+    };
+  };
+//Add 10 alien ships in random spots of the game wrap
+  function aliensAttack(){
+    var xmax = 250;
+    var xmin = 20;
+    var ymax = 650;
+    var ymin = 20;
+    for (var i=0; i<30; i++){
+      var x = Math.floor(Math.random() * (xmax - xmin + 1)) + xmin;
+      var y = Math.floor(Math.random() * (ymax - ymin + 1)) + ymin;
+      alienShip = new Alien(x, y).render();
     };
   };
 //Laser Object
   function Laser(){
     this.x = $('#ship').position().left + 14
-    this.y = 450 //$('#ship').position().top
+    this.y = 465 //$('#ship').position().top
     this.fire = function(){
       this.$me = $('<div class="laser"></div>')
       .css({
